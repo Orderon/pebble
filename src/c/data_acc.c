e95a6f759a3900c1aa13fb188e5eacf0846d9645
Orderon <cyrill.baumann@epfl.ch>
2016-10-24 17:02:14 +0200

    dodododood

--- src/c/data_acc.c
+++ src/c/data_acc.c
@@ -27,6 +27,13 @@ void data_norm_init(Data_Norm * Norm)
 void data_acc_update_acc(AccelData * Data, Data_Acc * Acc[3], uint32_t num_samples)
 {
   uint32_t i = 0;
+  for (i = 0; i <= 6; i++)
+  {
+    Acc[X]->last_acc[6-1-i] =  Acc[X]->new_acc[num_samples-1-i];
+    Acc[Y]->last_acc[6-1-i] =  Acc[Y]->new_acc[num_samples-1-i];
+    Acc[Z]->last_acc[6-1-i] =  Acc[Z]->new_acc[num_samples-1-i];
+  }
+  
   for (i = 0; i <= num_samples - 1; i++)
   {
     // Enregistrement des accélérations
@@ -34,9 +41,4 @@ void data_acc_update_acc(AccelData * Data, Data_Acc * Acc[3], uint32_t num_sampl
     Acc[Y]->new_acc[i] = Data[i].y;
     Acc[Z]->new_acc[i] = Data[i].z;
   }
-}
-
-int data_acc_get_samples_x(Data_Acc * Acc, int indice, int indice_coord)
-{
-   return 1;
 }
--- src/c/data_acc.h
+++ src/c/data_acc.h
@@ -26,8 +26,3 @@ void data_norm_init(Data_Norm * Norm);
 
 //Enregistre les "num_samples" accélérations lue par l'accéléromètre dans la structure Acc
 void data_acc_update_acc(AccelData * Data, Data_Acc * Acc[3], uint32_t num_samples);
-
-/* Retourne une valeure d'accélération choisie de manière suivante :
-    - indice : 0 -> dernière accélération, 1 -> avant dernière ...
-    - indice_ccord : 0 -> suivant l'axe x, 1 -> y, autre -> z        */
-int data_acc_get_samples(Data_Acc * Acc, int indice, int indice_coord);
